06ae9f21d2871517ee7c975c3b2decc0790aa727
Kelvin <ducter@163.com>
2016-07-21 19:07:57 +0800

    上传ray框架的基本结构

---
+++ src/main/java/com/effvi/ray/App.java
@@ -0,0 +1,13 @@
+package com.effvi.ray;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
