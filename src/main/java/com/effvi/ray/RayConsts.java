2f2add9e263cadf138da1a18b1963082a77736ef
Kelvin <38599125@qq.com>
2016-07-22 05:15:56 +0800

    创建web入口过滤器  RayFilter

---
+++ src/main/java/com/effvi/ray/RayConsts.java
@@ -0,0 +1,13 @@
+package com.effvi.ray;
+
+/**
+ * 
+ * <p>Title:常量类</p>
+ * <p>Description:</p>
+ * @author <a href="mailto:ducter@163.com">Kelvin</a>
+ * @date 2016年7月22日 上午5:08:05
+ *
+ */
+public class RayConsts {
+
+}
---
+++ src/main/java/com/effvi/ray/RayFilter.java
@@ -0,0 +1,34 @@
+package com.effvi.ray;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.filter.GenericFilterBean;
+
+/**
+ * 
+ * <p>Title:Ray框架致力于解决80%的Java Web开发场景遇到的问题</p>
+ * <p>Description:</p>
+ * @author <a href="mailto:ducter@163.com">Kelvin</a>
+ * @date 2016年7月21日 下午7:12:27
+ *
+ */
+public class RayFilter extends GenericFilterBean {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		
+		final HttpServletRequest req = (HttpServletRequest)request;
+		final HttpServletResponse rsp = (HttpServletResponse)response;
+		
+		
+	}
+	
+}
