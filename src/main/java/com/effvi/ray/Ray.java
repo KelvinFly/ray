01db26f370bc2d87eeeac897bdf7b4d51fc348ee
Kelvin <ducter@163.com>
2016-07-22 03:52:11 +0800

    第一个ray类

---
+++ src/main/java/com/effvi/ray/Ray.java
@@ -0,0 +1,13 @@
+package com.effvi.ray;
+
+/**
+ * 
+ * <p>Title:Ray框架致力于解决80%的Java Web开发场景遇到的问题</p>
+ * <p>Description:</p>
+ * @author <a href="mailto:ducter@163.com">Kelvin</a>
+ * @date 2016年7月21日 下午7:12:27
+ *
+ */
+public class Ray {
+
+}
