8d58f580e006f320eaf839554c504e228a1b820e
Kelvin.Zhang <38599125@qq.com>
2016-08-03 17:33:41 +0800

    RayFilter增加模块初始化相关代码

--- src/main/java/com/effvi/ray/RayFilter.java
+++ src/main/java/com/effvi/ray/RayFilter.java
@@ -2,6 +2,7 @@ package com.effvi.ray;
 
 import java.io.IOException;
 import java.util.Enumeration;
+import java.util.List;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -10,8 +11,12 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.GenericFilterBean;
+
+import com.effvi.ray.web.context.ContextScope;
+import com.effvi.ray.web.context.RayWebApplicationContext;
 
 /**
  * 
@@ -28,6 +33,13 @@ import org.springframework.web.filter.GenericFilterBean;
  */
 public class RayFilter extends GenericFilterBean {
 
+	private static final String ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE = WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE;
+
+	private String contextConfigLocation; // applicationContext路径
+
+	private ContextScope scope; // 上下文环境范围
+	
+//	private List<RayModule> modules; //Ray模块
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
@@ -70,16 +82,70 @@ public class RayFilter extends GenericFilterBean {
 			}
 			logger.debug("[Ray Init] init pramater " + sb);
 		}
-		
+		// 初始化Ray上下文环境
 		WebApplicationContext rootContext = initWebApplicationContext();
+		
+		if(logger.isInfoEnabled()){
+			logger.info("[Ray Init] exit from 'init/rootCotnext'");
+			logger.info("[Ray Init] exit from 'init/loadModule'");
+		}
+		
+//		this.modules = loadModule(rootContext);
+		
 		super.initFilterBean();
 	}
 	
+	/**
+	 * 初始化Ray上下文环境
+	 * @return
+	 */
 	private WebApplicationContext initWebApplicationContext() {
+		if (logger.isInfoEnabled()) {
+			logger.info("[Ray Init] 'init/rootContext' starting...");
+		}
+		RayWebApplicationContext rootContext = new RayWebApplicationContext(
+				getServletContext(), scope, false);
+
+		String cotextConfigLocation = this.contextConfigLocation;
+		if (StringUtils.isBlank(cotextConfigLocation)) { // 优先使用contextConfigLocation的配置
+			String webxmlContextConfigLocation = getServletContext()
+					.getInitParameter("contextConfigLocation");
+			if (StringUtils.isBlank(webxmlContextConfigLocation)) {
+				contextConfigLocation = RayWebApplicationContext.DEFAULT_CONFIG_LOCATION;
+			} else {
+				cotextConfigLocation = webxmlContextConfigLocation;
+			}
+		}
+
+		rootContext.setConfigLocation(cotextConfigLocation);
+		rootContext.setId("ray.web.root");
+		rootContext.refresh();
+
+		if (logger.isInfoEnabled()) {
+			logger.info("[Ray Init] 'init/rootContext' exit");
+		}
+
+		getServletContext().setAttribute(
+				ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE, rootContext);
+
+		if (logger.isInfoEnabled()) {
+			StringBuilder sb = new StringBuilder();
+			sb.append(
+					"[Ray Init] 'init/rootContext'  Start ray.web.root WebApplicationContext [")
+					.append(rootContext)
+					.append("] as ServletContext attribute with name [")
+					.append(ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE).append("]");
+			logger.info("[Ray Init] 'init/rootContext'  Start ray.web.root WebApplicationContext");
+		}
+		return rootContext;
+	}
+	 
+	private void loadModule(WebApplicationContext rootContext){
 		if(logger.isInfoEnabled()){
-			logger.info("[Ray Init] call 'init/rootContext' starting...");
+			logger.info("[Ray Init] 'init/loadModule' starting...");
 		}
 		
-		return null;
+		
 	}
+	
 }
