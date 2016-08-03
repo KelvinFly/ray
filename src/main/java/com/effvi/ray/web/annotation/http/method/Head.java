76da336a5de303c9f94810075feac0c5e53b0931
Kelvin <38599125@qq.com>
2016-08-03 08:46:08 +0800

    增加Http 8种请求方式对应的注解

---
+++ src/main/java/com/effvi/ray/web/annotation/http/method/Head.java
@@ -0,0 +1,23 @@
+package com.effvi.ray.web.annotation.http.method;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 
+ * <p>Title:Http协议的Head注解</p>
+ * <p>Description:注解所标记的方法可以处理Htpp Head请求</p>
+ * @author <a href="mailto:ducter@163.com">Kelvin</a>
+ * @date 2016年8月2日 下午5:48:47
+ *
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Head {
+	
+	String[] value() default "";
+}
